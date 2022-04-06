4eeb67f3791a225a545c97db3de367af7a942f97
wangguopen1121 <6202470@qq.com>
2022-04-06 12:54:05 +0800

    dev

---
+++ src/componente/AAA.js
@@ -0,0 +1,14 @@
+import React ,{Component} from 'react';
+class AAA extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <div className="sss"></div>
+        );
+    }
+}
+
+export default AAA;
